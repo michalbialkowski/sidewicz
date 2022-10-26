1e3e01acd97f88f6508f1cb34ddd32b5c63cf75d
Michał Białkowski <michal@bialkowski.net.pl>
2022-10-26 16:04:07 +0200

    Update

--- assets/js/bootstrap/triggers/toast.js
+++
@@ -1,9 +0,0 @@
-const toastTrigger = document.getElementById('toast_live_btn')
-const toastLiveExample = document.getElementById('toast_live')
-if (toastTrigger) {
-        toastTrigger.addEventListener('click', () => {
-        const toast = new bootstrap.Toast(toastLiveExample)
-
-        toast.show()
-    })
-}
